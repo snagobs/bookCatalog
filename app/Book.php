c44b64662691f26f7713fd8f65b933fdb56cd6f4
Serhii Nachosov <snagobs@gmail.com>
2020-02-09 20:02:48 +0200

    Reformat code

--- app/Book.php
+++ app/Book.php
@@ -10,14 +10,4 @@ class Book extends Model
     protected $primaryKey = 'id';
     protected $fillable = ['title', 'author', 'genre', 'price', 'description','created_at','updated_at'];
 
-    public function author()
-    {
-        return $this->belongsTo(Author::class);
-    }
-
-    public function genre()
-    {
-        return $this->belongsTo(Genre::class);
-    }
-
 }
